6929edf1af1c1d32494366f218b38778dbb5030e
antrhizom <151164845+antrhizom@users.noreply.github.com>
2025-11-18 16:38:35 +0100

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,1 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDPWkwiCptXi_Ky-yUgrvxrtpRzH1HGd_0",
+  authDomain: "abu-edu-2030.firebaseapp.com",
+  projectId: "abu-edu-2030",
+  storageBucket: "abu-edu-2030.firebasestorage.app",
+  messagingSenderId: "895317724223",
+  appId: "1:895317724223:web:de551267b1431bea947c8a"
+};
+
+export default firebaseConfig;
+window.firebaseConfig = firebaseConfig; // macht es auch für code-login.html sichtbar
 
